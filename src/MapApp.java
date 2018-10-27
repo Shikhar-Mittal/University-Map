6ab85e0eec860562a5f107b07fe52d1d42ca3a44
Shikhar Mittal <smittal9@wisc.edu>
2018-10-27 15:12:33 -0500

    Add files via upload

---
+++ src/MapApp.java
@@ -0,0 +1,305 @@
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS367 Spring 2016
+// PROJECT:          p4
+// FILE:             Interval.java
+//
+// TEAM:    Team 6
+// Authors:
+// Author1: (Sidney Smith, sbsmith5@wisc.edu, sbsmith5, 001)
+// Author2: (Jack Weisburg, weissburg2@wisc.edu, weissburg2, 002)
+//
+//////////////////////////// 80 columns wide //////////////////////////////////
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Driver class that reads/parses the input file and creates NavigationGraph
+ * object.
+ * 
+ * @author CS367
+ *
+ */
+public class MapApp {
+
+	private NavigationGraph graphObject;
+
+	/**
+	 * Constructs a MapApp object
+	 * 
+	 * @param graph
+	 *            NaviagtionGraph object
+	 */
+	public MapApp(NavigationGraph graph) {
+		this.graphObject = graph;
+	}
+
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.out.println("Usage: java MapApp <pathToGraphFile>");
+			System.exit(1);
+		}
+
+		// read the filename from command line argument
+		String locationFileName = args[0];
+		try {
+			NavigationGraph graph = createNavigationGraphFromMapFile(locationFileName);
+			MapApp appInstance = new MapApp(graph);
+			appInstance.startService();
+
+		} catch (FileNotFoundException e) {
+			System.out.println("GRAPH FILE: " + locationFileName + " was not found.");
+			System.exit(1);
+		} catch (InvalidFileException e) {
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+
+	}
+
+	/**
+	 * Displays options to user about the various operations on the loaded graph
+	 */
+	public void startService() {
+
+		System.out.println("Navigation App");
+		Scanner sc = new Scanner(System.in);
+
+		int choice = 0;
+		do {
+			System.out.println();
+			System.out.println("1. List all locations");
+			System.out.println("2. Display Graph");
+			System.out.println("3. Display Outgoing Edges");
+			System.out.println("4. Display Shortest Route");
+			System.out.println("5. Quit");
+			System.out.print("Enter your choice: ");
+
+			while (!sc.hasNextInt()) {
+				sc.next();
+				System.out.println("Please select a valid option: ");
+			}
+			choice = sc.nextInt();
+
+			switch (choice) {
+			case 1:
+				System.out.println(graphObject.getVertices());
+				break;
+			case 2:
+				System.out.println(graphObject.toString());
+				break;
+			case 3: {
+				System.out.println("Enter source location name: ");
+				String srcName = sc.next();
+				Location src = graphObject.getLocationByName(srcName);
+
+				if (src == null) {
+					System.out.println(srcName + " is not a valid Location");
+					break;
+				}
+
+				List<Path> outEdges = graphObject.getOutEdges(src);
+				System.out.println("Outgoing edges for " + src + ": ");
+				for (Path path : outEdges) {
+					System.out.println(path);
+				}
+			}
+				break;
+
+			case 4:
+				System.out.println("Enter source location name: ");
+				String srcName = sc.next();
+				Location src = graphObject.getLocationByName(srcName);
+
+				System.out.println("Enter destination location name: ");
+				String destName = sc.next();
+				Location dest = graphObject.getLocationByName(destName);
+
+				if (src == null || dest == null) {
+					System.out.println(srcName + " and/or " + destName + " are not valid Locations in the graph");
+					break;
+				}
+
+				if (src == dest) {
+					System.out.println(srcName + " and " + destName + " correspond to the same Location");
+					break;
+				}
+
+				System.out.println("Edge properties: ");
+				// List Edge Property Names
+				String[] propertyNames = graphObject.getEdgePropertyNames();
+				for (int i = 0; i < propertyNames.length; i++) {
+					System.out.println("\t" + (i + 1) + ": " + propertyNames[i]);
+				}
+				System.out.println("Select property to compute shortest route on: ");
+				int selectedPropertyIndex = sc.nextInt() - 1;
+
+				if (selectedPropertyIndex >= propertyNames.length) {
+					System.out.println("Invalid option chosen: " + (selectedPropertyIndex + 1));
+					break;
+				}
+
+				String selectedPropertyName = propertyNames[selectedPropertyIndex];
+				List<Path> shortestRoute = graphObject.getShortestRoute(src, dest, selectedPropertyName);
+				for(Path path : shortestRoute) {
+					System.out.print(path.displayPathWithProperty(selectedPropertyIndex)+", ");
+				}
+				if(shortestRoute.size()==0) {
+					System.out.print("No route exists");
+				}
+				System.out.println();
+
+				break;
+
+			case 5:
+				break;
+
+			default:
+				System.out.println("Please select a valid option: ");
+				break;
+
+			}
+		} while (choice != 5);
+		sc.close();
+	}
+
+	/**
+	 * Reads and parses the input file passed as argument create a
+	 * NavigationGraph object. The edge property names required for
+	 * the constructor can be got from the first line of the file
+	 * by ignoring the first 2 columns - source, destination. 
+	 * Use the graph object to add vertices and edges as
+	 * you read the input file.
+	 * 
+	 * @param graphFilepath
+	 *            path to the input file
+	 * @return NavigationGraph object
+	 * @throws FileNotFoundException
+	 *             if graphFilepath is not found
+	 * @throws InvalidFileException
+	 *             if header line in the file has < 3 columns or 
+	 *             if any line that describes an edge has different number of properties 
+	 *             	than as described in the header or 
+	 *             if any property value is not numeric 
+	 */
+
+	public static NavigationGraph createNavigationGraphFromMapFile(String graphFilepath) throws FileNotFoundException, InvalidFileException{
+			// TODO: read/parse the input file graphFilepath and create
+			// NavigationGraph with vertices and edges
+					//String[] edgePropertyNames = new String[10]; //TODO: arrayList
+		if(graphFilepath == null)
+			throw new FileNotFoundException();
+		
+		//array list of the edge names
+		List<String> edgeNamesList = new ArrayList<String>();
+		
+		
+		//Scanner for string
+		Scanner scnr = new Scanner(graphFilepath); 
+		String headers = scnr.nextLine(); 
+		//Scanner for header labels (first line)
+		Scanner firstLine = new Scanner(headers); 
+		firstLine.next(); //skips "source"
+		firstLine.next(); //skips "destination"
+		int propertyCounter = 0;
+		while (firstLine.hasNext()){
+			//adds property names from file
+			edgeNamesList.add(firstLine.next()); 
+			propertyCounter++;
+		}
+		//if header line is too short
+		if (propertyCounter < 2){
+			throw new InvalidFileException("header line in the file has < 3 columns");
+		}
+		
+		//converts ArrayList of property names to Array
+		String[] edgePropertyNames = new String [edgeNamesList.size()];
+		for(int k = 0; k < edgeNamesList.size(); k++){
+				edgePropertyNames[k] = edgeNamesList.get(k);
+		}
+			
+		//navGraph that will add vertices and edges
+		NavigationGraph navGraph = new NavigationGraph(edgePropertyNames);
+		
+		//arrayList of the location
+		List<Location> locationList = new ArrayList<Location>();
+
+		int i = 0; //i is used for the GraphNode IDs
+		while (scnr.hasNext()){
+			//list for edge values
+			List<Double> edges = new ArrayList<Double>(edgeNamesList.size()); 
+			String source;
+			String destination;
+			Boolean sourceDup = false;
+			Boolean destDup = false;
+			source = scnr.next();
+			destination = scnr.next();
+			Location sourceLoc = new Location(source);
+			Location destinationLoc = new Location(destination);
+			
+			for(int j = 0; j < locationList.size(); j++){
+				if (source.equals(locationList.get(j))){
+					sourceDup = true;
+				}
+				if (destination.equals(locationList.get(i))){
+					destDup = true;
+				}
+				if (source.equals(destination)){
+					destDup = true;
+					sourceDup = true;
+				}
+			}
+			if(!sourceDup){
+				//creates new vertex if it isn't a duplicate
+				navGraph.addVertex(sourceLoc);
+				i++;
+				//adds location to the list
+				locationList.add(sourceLoc);
+			}
+			if(!destDup){
+				navGraph.addVertex(destinationLoc);
+				i++;
+				locationList.add(destinationLoc);
+			}
+			
+			//create edge
+			Path newPath = new Path(sourceLoc, destinationLoc, edges);
+			navGraph.addEdge(sourceLoc, destinationLoc, newPath);
+			
+			//adds edge weight if it is numeric
+			int intTemp = 0; //used if weight is an int
+			//keep track of how many properties are on each line
+			int numOfProperties = 0;
+			while(scnr.hasNextLine()){
+				if (scnr.hasNextDouble()){
+					edges.add(scnr.nextDouble());
+					numOfProperties++;
+				}
+				else if (scnr.hasNextInt()) {
+					intTemp = scnr.nextInt();
+					edges.add((double) intTemp);
+					numOfProperties++;
+				}
+				else {
+					throw new InvalidFileException("property value is not numeric");
+				}	
+			}
+			//if the number of properties is inconsistent, throw error
+			if (numOfProperties != propertyCounter)
+				throw new InvalidFileException("line that describes an edge has"
+						+ " different number of properties than as described in the header");
+			
+			scnr.nextLine(); //skip to next line
+		}
+		
+		
+		//returns the navigation graph
+		return navGraph;
+
+	}
+
+}
---
+++ src/NavigationGraph.java
@@ -0,0 +1,346 @@
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS367 Spring 2016
+// PROJECT:          p4
+// FILE:             Interval.java
+//
+// TEAM:    Team 6
+// Authors:
+// Author1: (Sidney Smith, sbsmith5@wisc.edu, sbsmith5, 001)
+// Author2: (Jack Weissburg,,,)
+// Author3: (Patrick Budyn, budyn@wisc.edu, budyn, 002)
+// Author4: (Shikhar Mittal,,,)
+//
+//////////////////////////// 80 columns wide //////////////////////////////////
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class NavigationGraph implements GraphADT<Location, Path> {
+
+
+	private String[] edgeProperties;
+	//SID - Will be representing the graph as an ArrayList<LinkedList<TYPE (TBD)>>
+	private ArrayList< GraphNode<Location, Path>> navigationGraph;
+	//this represents the list of locations found in this NavigationGraph instance
+	private ArrayList<Location> vertices;
+	//this represents the list of edges found in this NavigationGraph instance.
+	//created to make getEdgeIfExists() easier to implement.
+	private ArrayList<Path> edges;
+	//this represents the current id to be assigned to new GraphNodes
+	private int id = 0;
+
+	/**
+	 * Creates a directed NavigationGraph object whose paths have edge properties 
+	 * 
+	 * @param edgePropertyNames
+	 *            properties that edges within this NavigationGraph consist of
+	 * @param destination
+	 *            destination location for the edge
+	 * @param pathProperties
+	 *            list of property values associated with the edge (Note: length
+	 *            of this list must agree with the propertynames in the graph)
+	 */
+	public NavigationGraph(String[] edgePropertyNames) {
+		//TODO - Done (Sid)
+		if(edgePropertyNames == null)
+			throw new IllegalArgumentException();
+		
+		this.edgeProperties = edgePropertyNames;	
+	}
+
+
+
+	/**
+	 * Adds a vertex to the Graph
+	 * 
+	 * @param vertex
+	 *            vertex to be added
+	 */
+	public void addVertex(Location vertex){
+		// TODO - Done (Sid)
+		// Checked (PB & JW)
+
+		//scan the list of current vertices in the graph to make sure that the vertex to be added
+		//does not already exist
+		if (vertex == null)
+			throw new IllegalArgumentException();
+		for (int i = 0; i<this.vertices.size(); i++)
+			if (this.vertices.get(i).equals(vertex))
+				return;
+
+		GraphNode<Location, Path> toAdd = new GraphNode<Location, Path>(vertex, this.id);
+		
+
+		vertices.add(vertex);
+		this.navigationGraph.add(toAdd);
+		this.id++;
+		return;
+	}
+
+	/**
+	 * Creates a directed edge from src to dest
+	 * 
+	 * @param src
+	 *            source vertex from where the edge is outgoing
+	 * @param dest
+	 *            destination vertex where the edge is incoming
+	 * @param edge
+	 *            edge between src and dest
+	 */
+	public void addEdge(Location src, Location dest, Path edge){
+		//TODO - DONE (SID)
+		//	  	 Checked ()
+		// do we need to add a check to see if there is path from src to dest.
+
+		if(src == null || dest == null || edge == null)
+			throw new IllegalArgumentException();
+		GraphNode<Location, Path> g = getGraphNodebyLocation(src);
+		
+		if(g == null){
+			throw new NullPointerException();
+		}
+		
+		g.addOutEdge(edge);
+		this.edges.add(edge); //adds edge
+		this.navigationGraph.get(g.getId()).addOutEdge(edge);
+		return;
+	}
+
+	private GraphNode<Location, Path> getGraphNodebyLocation(Location src){
+
+		//TODO optimize this code. Do not create a graphnode every loop iteration
+		for(int j=0; j<navigationGraph.size();j++){
+			GraphNode<Location, Path> temp = (navigationGraph.get(j));
+			if(temp.getVertexData().equals(src)){
+				return temp;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Getter method for the vertices
+	 * 
+	 * @return List of vertices of type Location
+	 */
+	public List<Location> getVertices(){
+		//TODO - Done (SID)
+		//		 Checked (PB & JW)
+		return this.vertices;
+	}
+
+	/**
+	 * Returns edge if there is one from src to dest vertex else null
+	 * 
+	 * @param src
+	 *            Source vertex
+	 * @param dest
+	 *            Destination vertex
+	 * @return Edge of type Path from src to dest
+	 */
+	public Path getEdgeIfExists(Location src, Location dest){
+		//TODO - Done (SID)
+		//		 Checked (PB & JW)
+		for (int j = 0; j<edges.size(); j++){
+			//if the source is the same, check the destination
+			if (edges.get(j).getSource().equals(src)){
+				//if the destination is the same as well, return the existing edge
+				if (edges.get(j).getDestination().equals(dest)){
+					return edges.get(j);
+				}
+			}
+		}
+		//else, an edge does not exist
+		return null;
+	}
+
+	/**
+	 * Returns the outgoing edges from a vertex
+	 * 
+	 * @param src
+	 *            Source vertex for which the outgoing edges need to be obtained
+	 * @return List of edges of type Path
+	 */
+	public List<Path> getOutEdges(Location src) 
+			throws IllegalArgumentException{
+		//TODO - Done (SID)
+		//		 Checked (PB & JW)
+		GraphNode<Location, Path> g = getGraphNodebyLocation(src);
+		if (g!=null)
+			return g.getOutEdges();
+		//if the vertex does not exist, throw illegalArgumentException
+		throw new IllegalArgumentException("That location does not exist!");
+	}
+
+	/**
+	 * Returns neighbors of a vertex
+	 * 
+	 * @param vertex
+	 *            vertex for which the neighbors are required
+	 * @return List of vertices(neighbors) of type Location
+	 */
+	public List<Location> getNeighbors(Location vertex){
+		
+		if (vertex == null)
+			throw new IllegalArgumentException();
+		
+		List<Location> neighbors = new ArrayList<Location>();
+		List<Path> vertexPaths = new ArrayList<Path>();
+
+		//check all nodes for neighbors or vertex
+		for(int i = 0; i < vertexPaths.size(); i++){
+			if(navigationGraph.get(i).equals(vertex)){
+				//"out" neighbors
+				List<Path> temp = navigationGraph.get(i).getOutEdges();
+				for(int j = 0; j < temp.size(); j++){
+					neighbors.add(temp.get(j).getSource());
+				}
+			}
+			//"in" neighbors
+			if(navigationGraph.get(i).getOutEdges().contains(vertex)){
+				neighbors.add(navigationGraph.get(i).getVertexData());
+			}
+		}
+
+		return neighbors;
+	}
+
+
+	/**
+	 * Calculate the shortest route from src to dest vertex using
+	 * edgePropertyName
+	 * 
+	 * @param src
+	 *            Source vertex from which the shortest route is desired
+	 * @param dest
+	 *            Destination vertex to which the shortest route is desired
+	 * @param edgePropertyName
+	 *            edge property by which shortest route has to be calculated
+	 * @return List of paths that denote the shortest route by edgePropertyName
+	 */
+	public List<Path> getShortestRoute(Location src, Location dest, String edgeProperty)
+		throws IllegalArgumentException{
+		//make static shell class with predecessor list equal to null, weight equal to infinity, and visited to false
+		//in algorithm, have priority queue with vertex info of locations, put weight equal to 0
+		//call get vertices //this.getvertices
+		
+		if (src == null || dest == null || edgeProperty == null)
+			throw new IllegalArgumentException();
+		
+		List<Path> shortestPath = new ArrayList<Path>();
+		
+		QueueEntry[] queue = new QueueEntry[navigationGraph.size()];
+		int indexOfProperty = 0;
+		
+		//get index for weight property to be used for shortest route
+		for (int i = 0; i<edgeProperties.length; i++){
+			if(edgeProperties[i].equals(edgeProperty)){
+				indexOfProperty = i;
+				break;
+			}
+		}
+		
+		//create a queue entry for each vertex
+		for (int i = 0; i<navigationGraph.size(); i++){
+			
+			//get all vertices in the graph, place them in the list of queue entries
+			queue[i] = new QueueEntry(Double.MAX_VALUE, navigationGraph.get(i));
+			//set each entry to unvisited
+			queue[i].setVisited(false);
+			//initialize each entry's predecessor to null
+			queue[i].setPredecessor(null);
+		}
+		
+		
+		int startIndex = getIdByLocation(src);
+		queue[startIndex].setWeight(0.0);
+		PriorityQueue<QueueEntry> pq = new PriorityQueue<QueueEntry>();
+		pq.add(queue[startIndex]);
+		
+		//current entry, 'C' as in Dijkstra's algorithm in class notes
+		QueueEntry curr;
+		while(!pq.isEmpty()){
+			
+			//remove min from queue
+			curr = pq.poll();
+			curr.setVisited(true);
+			
+			for (int j=0; j<curr.getVertex().getOutEdges().size(); j++){
+				Path successor = curr.getVertex().getOutEdges().get(j);
+				//get successor location in navgraph for the node that corresponds to the successor
+				int index = getIdByLocation(successor.getDestination());
+				
+				if (!queue[index].getVisited()){
+					if (curr.getWeight() + successor.getProperties().get(indexOfProperty) < queue[index].getWeight()){
+						queue[index].setWeight(curr.getWeight() + successor.getProperties().get(indexOfProperty));
+						queue[index].setPredecessor(curr);
+						if (!pq.contains(queue[index])){
+							pq.add(queue[index]);
+						}
+					}
+				}
+			}
+		}
+		
+		//get destination
+		int destIndex = getIdByLocation(dest);
+		QueueEntry current = queue[destIndex];
+		while (current.getPredecessor() != null){
+			Path predEdge = getEdgeIfExists(current.getPredecessor().getVertex().getVertexData(), current.getVertex().getVertexData());
+			//only add non-null
+			if (predEdge != null){
+				shortestPath.add(0, predEdge);
+			}
+			current = current.getPredecessor();
+		}
+		
+		return shortestPath;
+
+	}
+
+	/**
+	 * Getter method for edge property names
+	 * 
+	 * @return array of String that denotes the Path property names
+	 */
+	public String[] getEdgePropertyNames(){
+		return edgeProperties ;
+
+	}
+
+
+
+	/**
+	 * Returns a Location object given its name
+	 * 
+	 * @param name
+	 *            name of the location
+	 * @return Location object
+	 */
+	public Location getLocationByName(String name) {
+		
+		if (name == null)
+			throw new IllegalArgumentException();
+		
+		for (int k = 0; k < navigationGraph.size(); k++){
+			if (vertices.get(k).getName().equals(name)){
+				return vertices.get(k);
+			}
+		}
+		return null;
+
+	}
+	
+	public int getIdByLocation(Location name) 
+		throws IllegalArgumentException{
+		
+		for (int j = 0; j<navigationGraph.size(); j++){
+			if (navigationGraph.get(j).getVertexData().equals(name)){
+				return navigationGraph.get(j).getId();
+			}
+		}
+		throw new IllegalArgumentException();
+	}
+
+}
